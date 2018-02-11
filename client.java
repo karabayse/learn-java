bf4cbeca177a7c5eccdcd43741f3acaaa0bce9bd
Kara Bayse <karabayse@gmail.com>
2018-02-11 13:37:40 -0600

    ArrayList

--- client.java
+++ client.java
@@ -320,3 +320,16 @@ public class For {
 	}
 
 }
+
+
+// ARRAYLIST
+import java.util.ArrayList;
+
+public class Temperatures {
+
+	public static void main(String[] args) {
+
+    	ArrayList<Integer> weeklyTemperatures = new ArrayList<Integer>();
+
+	}
+}
