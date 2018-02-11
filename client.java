40db728524a468d67aad6b9284493ee211083ba7
Kara Bayse <karabayse@gmail.com>
2018-02-11 13:35:18 -0600

    for loop

--- client.java
+++ client.java
@@ -249,3 +249,74 @@ class Vehicle {
 
     }
 }
+
+
+
+
+import java.util.*;
+
+public class Olympics {
+
+	public static void main(String[] args) {
+
+		//Some Olympic sports
+
+		ArrayList<String> olympicSports = new ArrayList<String>();
+		olympicSports.add("Archery");
+		olympicSports.add("Boxing");
+		olympicSports.add("Cricket");
+		olympicSports.add("Diving");
+
+		System.out.println("There are " + olympicSports.size() + " Olympic sports in this list. They are: ");
+
+		for (String sport: olympicSports) {
+			System.out.println(sport);
+		}
+
+		//Host cities and the year they hosted the summer Olympics
+
+		HashMap<String, Integer> hostCities = new HashMap<String, Integer>();
+
+		hostCities.put("Beijing", 2008);
+		hostCities.put("London", 2012);
+		hostCities.put("Rio de Janeiro", 2016);
+
+		for (String city: hostCities.keySet()) {
+
+			if (hostCities.get(city) < 2016) {
+
+				System.out.println(city + " hosted the summer Olympics in " + hostCities.get(city) + ".");
+
+			} else {
+
+				System.out.println(city + " will host the summer Olympics in " + hostCities.get(city) + ".");
+
+			}
+		}
+
+	}
+
+}
+
+
+
+
+// FOR LOOP
+for (int counter = 0; counter < 5; counter++) {
+
+    System.out.println("The counter value is: " + counter);
+
+}
+
+public class For {
+	public static void main(String[] args) {
+
+		for (int waterLevel = 0; waterLevel < 7; waterLevel++) {
+
+			System.out.println("The pool's water level is at " + waterLevel + " feet.");
+
+		}
+
+	}
+
+}
